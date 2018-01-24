041ec3da08cd5854cd080906766a013afadfa423
Alice <wheresalice@noreply.github.com>
2018-01-24 17:35:52 +0000

    add kaerast/rake-preview

---
+++ cmd/rake.go
@@ -0,0 +1,23 @@
+package cmd
+
+import "github.com/skybet/cali"
+import "github.com/docker/go-connections/nat"
+
+func init() {
+
+	command := cli.NewCommand("rake")
+	command.SetShort("Run rake preview")
+	command.SetLong(`Starts a container and runs rake preview.
+Examples:
+  To render the site locally.
+  # staticli rake
+  Any addtional flags sent to the rake command come after the --, e.g.
+  # staticli rake preview -- --future
+`)
+	command.BindFlags()
+
+	task := command.Task("kaerast/rake-preview")
+	task.HostConf.PublishAllPorts = true
+	// @todo the above publishes ports defined in the Dockerfile as `EXPOSE` on a random port.  We would prefer a defined port, however we don't understand enough Go to use task.HostConf.PortBindings
+	task.SetInitFunc(func(t *cali.Task, args []string) {})
+}
