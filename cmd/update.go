dc87b71cde3478933071c48a2db3ffb3046dbf20
Alice <wheresalice@noreply.github.com>
2018-03-19 10:37:27 +0800

    take @lmhd's nice status bar for update

--- cmd/update.go
+++ cmd/update.go
@@ -1,52 +1,29 @@
 package cmd
 
 import (
-	"fmt"
 	log "github.com/Sirupsen/logrus"
-	"github.com/inconshreveable/go-update"
 	"github.com/skybet/cali"
 	"github.com/staticli/staticli/lib"
-	"net/http"
-	"runtime"
 )
-
-func doUpdate(url string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	err = update.Apply(resp.Body, update.Options{})
-	if err != nil {
-		log.Errorf("Couldn't update staticli: %s", err)
-	}
-	return err
-}
 
 func init() {
 
 	command := cli.NewCommand("update")
-	command.SetShort("Update the current running version of staticli")
-
-	releaseUrl := "https://api.github.com/repos/staticli/staticli/releases/latest"
+	command.SetShort("Update the current running version of lucli")
 
 	var taskFunc cali.TaskFunc = func(t *cali.Task, args []string) {
-		log.Infof("you are running staticli v%s (%s) (%s)", lib.Version, lib.BuildTime, lib.BuildCommit)
+		lib.PrintVersion()
 
-		releaseData := lib.Release{}
-		lib.GetJson(releaseUrl, &releaseData)
-		if releaseData.Name == "" {
-			log.Error("Couldn't get latest version of staticli, do you have an internet connection?")
-		} else {
-			log.Infof("staticli v%s is the latest release", releaseData.Name)
-			if lib.Version != releaseData.Name {
-				log.Infof("updating staticli to v%s", releaseData.Name)
-				// @todo this url is subject to change so we should probably get it from the github json
-				updateUrl := fmt.Sprintf("https://github.com/staticli/staticli/releases/download/%s/staticli.%s.%s", releaseData.Name, runtime.GOOS, runtime.GOARCH)
-				doUpdate(updateUrl)
-				log.Info("update complete")
+		isLatestVersion, updateReleaseData, err := lib.IsLatestVersion()
+		if err != nil {
+			log.Fatalf("Unable to check for update: %s", err)
+		}
+
+		if !isLatestVersion {
+			err = lib.Update(updateReleaseData)
+			if err != nil {
+				log.Fatalf("Unable to update: %s", err)
 			}
-
 		}
 	}
 
--- cmd/version.go
+++ cmd/version.go
@@ -3,31 +3,30 @@ package cmd
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/skybet/cali"
+
 	"github.com/staticli/staticli/lib"
-	"runtime"
 )
 
 func init() {
 
 	command := cli.NewCommand("version")
-	command.SetShort("Show current staticli version")
-
-	releaseUrl := "https://api.github.com/repos/staticli/staticli/releases/latest"
+	command.SetShort("Which version are we running?")
 
 	var taskFunc cali.TaskFunc = func(t *cali.Task, args []string) {
-		log.Infof("staticli.%s.%s v%s (%s) (%s)", runtime.GOOS, runtime.GOARCH, lib.Version, lib.BuildTime, lib.BuildCommit)
+		lib.PrintVersion()
 
-		releaseData := lib.Release{}
-		lib.GetJson(releaseUrl, &releaseData)
-		if releaseData.Name != "" {
-			log.Infof("staticli v%s is the latest release", releaseData.Name)
-			if lib.Version != releaseData.Name {
-				log.Warnf("You should update your version of staticli")
-			}
+		isLatestVersion, releaseData, err := lib.IsLatestVersion()
+		if err != nil {
+			log.Fatalf("Unable to check for update: %s", err)
+		}
 
+		if !isLatestVersion {
+			log.Infof("You're not running the latest version 😱")
+			log.Infof("Update to v%s with: lucli update", releaseData.Name)
 		}
 	}
 
+	// Simple task, just runs a function
 	command.Task(taskFunc)
 
 }
